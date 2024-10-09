ab7fc452cfec1663229af0590af35474abbceb12
Jorge Elias Concepcion Lorenzo <111101748+george23elias@users.noreply.github.com>
2024-10-08 23:02:25 -0400

    .

---
+++ Components/Pages/Caculadora/Calculador.cs
@@ -0,0 +1,22 @@
+
+
+class Calculador
+{
+
+    double sumar(double a, double b)
+    {
+        return a + b;
+    }
+    double resta(double a, double b)
+    {
+        return a - b;
+    }
+    double Multiplicar(double a, double b)
+    {
+        return a * b;
+    }
+    double Dividir(double a, double b)
+    {
+        return a / b;
+    }
+}
